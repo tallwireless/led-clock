1bfe27650597d2d9ff867cdb2a9eb95d92bcb061
Charles Rumford <charlesr@tallwireless.com>
2020-04-23 16:23:50 -0400

    Flake8/Black Compiance

--- run-clock.py
+++ run-clock.py
@@ -9,7 +9,12 @@ import arrow
 class RunText(SampleBase):
     def __init__(self, *args, **kwargs):
         super(RunText, self).__init__(*args, **kwargs)
-        self.parser.add_argument("-t", "--text", help="The text to scroll on the RGB LED panel", default="Hello world!")
+        self.parser.add_argument(
+            "-t",
+            "--text",
+            help="The text to scroll on the RGB LED panel",
+            default="Hello world!",
+        )
         self.continumm = 0
         self.textColor = None
 
@@ -46,22 +51,35 @@ class RunText(SampleBase):
         stationary_pos = 1
         scrolling_pos = offscreen_canvas.width
         now = arrow.now()
-        my_text = [f"{now.hour:02d}:{now.minute:02d}",f"{now.month:02d}/{now.day:02d}"]
+        my_text = [f"{now.hour:02d}:{now.minute:02d}", f"{now.month:02d}/{now.day:02d}"]
         scrolling_text = ["Mischief Managed", "Hack the Planet!", "Do good."]
         st_len = len(scrolling_text)
         st_index = 0
-        count=0
-        fill = True
+        count = 0
         while True:
             offscreen_canvas.Clear()
-            offset=7
+            offset = 7
             for text in my_text:
-                graphics.DrawText(offscreen_canvas, stationary_font, stationary_pos, offset, self.textColor, text)
-                offset+=10
-            l = graphics.DrawText(offscreen_canvas, scrolling_font, scrolling_pos, offset, self.textColor, scrolling_text[st_index])
+                graphics.DrawText(
+                    offscreen_canvas,
+                    stationary_font,
+                    stationary_pos,
+                    offset,
+                    self.textColor,
+                    text,
+                )
+                offset += 10
+            new_pos = graphics.DrawText(
+                offscreen_canvas,
+                scrolling_font,
+                scrolling_pos,
+                offset,
+                self.textColor,
+                scrolling_text[st_index],
+            )
             scrolling_pos -= 1
-             
-            if (scrolling_pos + l < 0):
+
+            if scrolling_pos + new_pos < 0:
                 st_index += 1
                 if st_index >= st_len:
                     st_index = 0
@@ -69,10 +87,12 @@ class RunText(SampleBase):
 
             time.sleep(0.05)
             count += 1
-            if count%20 == 0:
+            if count % 20 == 0:
                 now = arrow.now()
-                my_text = [f"{now.hour:02d}:{now.minute:02d}",f"{now.month:02d}/{now.day:02d}"]
-
+                my_text = [
+                    f"{now.hour:02d}:{now.minute:02d}",
+                    f"{now.month:02d}/{now.day:02d}",
+                ]
 
             offscreen_canvas = self.matrix.SwapOnVSync(offscreen_canvas)
             self.update_text_color()
@@ -81,5 +101,5 @@ class RunText(SampleBase):
 # Main function
 if __name__ == "__main__":
     run_text = RunText()
-    if (not run_text.process()):
+    if not run_text.process():
         run_text.print_help()
