52543ea6c87bdfc6c07750957eafd494596cb965
Charles Rumford <charlesr@tallwireless.com>
2020-04-24 00:43:12 -0400

    Adding Scrolling Text with multiple messages

--- line.py
+++ line.py
@@ -69,14 +69,54 @@ class ScrollingLine(TextLine):
         super(ScrollingLine, self).__init__(*args, **kwargs)
         self.firstTime = True
         self.canvas_width = 0
+        self.completed = False
 
     def draw(self, canvas, offset):
         if self.firstTime is True:
             self.pos = canvas.width
-            self.canvas_with = self.pos
+            self.canvas_width = canvas.width
             self.firstTime = False
-        end_pos = super(ScrollingLine, self).draw(canvas, offset)
-        self.pos -= 1
-        if self.pos + end_pos <= 0:
+
+        if self.completed:
+
             self.pos = self.canvas_width
+            self.completed = False
+
+        end_pos = super(ScrollingLine, self).draw(canvas, offset)
+        print(end_pos, self.pos)
+        self.pos -= 1
+
+        if self.pos + end_pos <= 0:
+            self.completed = True
+
         return end_pos
+
+
+class MultiScrollingLine(object):
+    def __init__(self, *args, **kwargs):
+        self.firstTime = True
+        self.canvas_width = 0
+        self.messages = []
+        self.height = 10
+        self.mpos = 0
+
+    def addMessage(self, message):
+        self.messages.append(message)
+
+    def draw(self, canvas, offset):
+        if len(self.messages) == 0:
+            raise MultiScrollingLineException("No ScrollingLine's Register")
+        end_pos = self.messages[self.mpos].draw(canvas, offset)
+        if self.messages[self.mpos].completed:
+            self.mpos += 1
+            if self.mpos >= len(self.messages):
+                self.mpos = 0
+        return end_pos
+
+    def update(self):
+        for message in self.messages:
+            message.update()
+
+
+class MultiScrollingLineException(Exception):
+    pass
