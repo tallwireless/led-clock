e97d8bff72c6ecd06d1ef86f9a49377cc2160dc1
Charles Rumford <charlesr@tallwireless.com>
2020-05-13 00:39:51 -0400

    adding some comments

--- screen.py
+++ screen.py
@@ -2,44 +2,65 @@ import time
 
 
 class END_SCREEN(object):
+    """When an element wants to signal it's done displaying content."""
+
     pass
 
 
 class Screen(object):
+    """A screen contains the actual content"""
+
     def __init__(self, count=100):
         self.elements = []
         self.display = None
         self.count = count
 
     def registerElement(self, element):
+        """add element to the screen to be displayed"""
         element.registerScreen(self)
         self.elements.append(element)
 
     def registerDisplay(self, display):
+        """allow the display to register itself"""
         self.display = display
 
     def run(self,):
+        """Actually display the content"""
         canvas = self.display.getCanvas()
         count = 0
         s = 0
+
+        # Center the elements on the display assuming a 32 pixel high display
         for element in self.elements:
             s += element.height
-
         start_offset = int((32 - s) / 2) - 1
+
         while True:
             canvas.Clear()
             offset = start_offset
+
+            # iterate through the elements have have them draw their conent
             for element in self.elements:
                 offset += element.height
                 rv = element.draw(canvas, offset)
+                # If the element has indicated it's time to end the screen,
+                # then we return to allow the display to move on to the next
+                # screen
                 if rv is END_SCREEN:
                     return
+                # spacing between elements
                 offset += 1
+
             time.sleep(0.03)
             count += 1
+
+            # have we hit our time limit yet?
             if count == self.count:
                 return ()
+
+            # give the elements a chance to update themselves
             if count % 20 == 0:
                 for element in self.elements:
                     element.update()
+
             canvas = self.display.matrix.SwapOnVSync(canvas)
